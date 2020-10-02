9e01e34875f00257139bb0e4eedff4a62c219a42
Andrey <asbogdanov13@gmail.com>
2020-10-02 20:36:42 +0500

    for check

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const burger = document.querySelector("#burger");
+const body = document.querySelector("body");
+
+burger.addEventListener ('click', verticalMenu);
+
+body.addEventListener ('click', hideVerticalMenu);
+
+function verticalMenu() {
+    var navMenu = document.querySelector("#nav");
+    navMenu.classList.toggle("nav-opened", true);
+  }
+
+function hideVerticalMenu () {
+    var navMenu = document.querySelector("#nav");
+    navMenu.classList.toggle ("nav-opened", false);
+}
+// --------- show answers -------
+const questionsAnswers = document.querySelectorAll(".question-answer-container");
+
+questionsAnswers.forEach(item => {
+    item.querySelector(".question").addEventListener('click', toggleAnswer);
+});
+
+function toggleAnswer (event) {
+    const toggledAnser = event.currentTarget.parentNode;
+    toggledAnser.classList.toggle("opened-question-answer");
+};
+// ---------------------------------------
